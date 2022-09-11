5b237f8f056c0f60db22b1b94174c9ca5ae5c790
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-09-11 13:32:35 +0200

    version controlling

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css'
+
+function App() {
+  return (
+    <>
+    <div>
+      <p>This is system sucks</p>
+    </div>
+    </>
+  );
+}
+
+export default App;
