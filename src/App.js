00bc96d385b192a99bf1cd17546a12aeaa90a2de
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-09-11 16:16:02 +0200

    making layout of tenzies game

--- src/App.js
+++ src/App.js
@@ -6,11 +6,27 @@ import './styles.css'
 
 function App() {
   return (
-    <>
-    <div>
-      <p>This is system sucks</p>
+    <main>
+    <div className='game-container'>
+      <div className='inner-tenzies'>
+        <h2 className='tenzies--title'>Tenzies</h2>
+        <p className='tenzies--instructions'>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+        <div className='tenzies-display'>
+          <div className='tenzies-number'>1</div>
+          <div className='tenzies-number'>2</div>
+          <div className='tenzies-number'>3</div>
+          <div className='tenzies-number'>4</div>
+          <div className='tenzies-number'>5</div>
+          <div className='tenzies-number'>6</div>
+          <div className='tenzies-number'>7</div>
+          <div className='tenzies-number'>8</div>
+          <div className='tenzies-number'>9</div>
+          <div className='tenzies-number'>10</div>
+        </div>
+        <button className='tenzies-btn'>Roll</button>
+      </div>
     </div>
-    </>
+    </main>
   );
 }
 
